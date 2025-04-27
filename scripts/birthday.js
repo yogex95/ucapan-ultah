0e44de55ca1586871b8da1c08c5a87ad67bd4275
Amin Prayoga <34791771+yogex95@users.noreply.github.com>
2025-04-27 21:56:59 +0700

    Update birthday.js

--- scripts/birthday.js
+++ scripts/birthday.js
@@ -103,7 +103,7 @@ const animationTimeline = () => {
     })
     .staggerTo(
       ".hbd-chatbox span",
-      1.5,
+      1.2,
       {
         visibility: "visible",
       },
@@ -131,14 +131,14 @@ const animationTimeline = () => {
     .to(".idea-1", 0.7, ideaTextTransLeave, "+=2.5")
     .from(".idea-2", 0.7, ideaTextTrans)
     .to(".idea-2", 0.7, ideaTextTransLeave, "+=2.5")
-    .from(".idea-3", 0.8, ideaTextTrans)
+    .from(".idea-3", 0.9, ideaTextTrans)
     .to(".idea-3 strong", 0.5, {
       scale: 0.9,
       x: 10,
       backgroundColor: "rgb(21, 161, 237)",
       color: "#fff",
     })
-    .to(".idea-3", 0.8, ideaTextTransLeave, "+=2.5")
+    .to(".idea-3", 0.9, ideaTextTransLeave, "+=2.5")
     .from(".idea-4", 0.7, ideaTextTrans)
     .to(".idea-4", 0.7, ideaTextTransLeave, "+=2.5")
     .from(
